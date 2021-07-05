92b0a6f0fcf6abaecd69e9184205be7acc09b74d
Donovan <don9617@hotmail.com>
2021-07-05 14:45:36 +0800

    Added user table and auth token for signup and login

--- backend/lifehack/views.py
+++ backend/lifehack/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .serializers import UserSerializer, TestSerializer
 from .models import User, Test
 
@@ -12,12 +10,3 @@ class UserView(viewsets.ModelViewSet):
 class TestView(viewsets.ModelViewSet):
     serializer_class = TestSerializer
     queryset = Test.objects.all()
-
-@api_view(['GET'])
-def current_user(request):
-    """
-    Determine the current user by their token, and return their data
-    """
-    
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
