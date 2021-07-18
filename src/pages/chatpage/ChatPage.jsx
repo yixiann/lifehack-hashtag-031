4847c0b5f609a61eba852be8f077b42f5dd80329
justinhoejj <hoejj05@gmail.com>
2021-07-18 13:47:53 +0800

    merge

--- src/pages/chatpage/ChatPage.jsx
+++ src/pages/chatpage/ChatPage.jsx
@@ -1,17 +1,54 @@
-import React, { useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import { Layout, Input, Button, Form, Row, Col } from 'antd'
+import { Header, Sider } from 'antd/lib/layout/layout'
 
 export const ChatPage = ({
   ...props
 }) => {
 
-  useEffect(() => {
-    console.log("TEST CHAT")
-  },[])
+  const [form] = Form.useForm()
+  const [selectedUserId, setSelectedUserId] = useState(null)
+  const [chatHistory, setChatHistory] = useState(null)
 
-    
+  useEffect(() => {
+    setSelectedUserId(1)
+    setChatHistory([
+      {
+        timeStamp: "moment1",
+        message: "Some long ass text send by 1",
+        sentBy: 1
+      },
+      {
+        timeStamp: "moment2",
+        message: "Some longggerr ass text send by me",
+        sentBy: 2
+      }
+    ])
+  }, [])
+
+
   return (
     <div className="chat">
-      <p>Chat</p>
+      <Form
+        form={form}
+        name="chat"
+      >
+        <Row>
+          <Col span={20}>
+            <Form.Item
+              name="message">
+              <Input 
+                placeholder="Input message"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item>
+              <Button>Send</Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
     </div>
   )
 }
