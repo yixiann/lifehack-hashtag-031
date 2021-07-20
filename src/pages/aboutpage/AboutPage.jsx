3a0ca8a88390841183c6f60300c32d4dd1a4dee2
Yi Xian <yixianns@gmail.com>
2021-07-20 14:42:41 +0800

    Image

--- src/pages/aboutpage/AboutPage.jsx
+++ src/pages/aboutpage/AboutPage.jsx
@@ -1,17 +1,38 @@
-import React, { useEffect} from 'react'
+import React, { useState } from 'react'
+import { Upload, Button } from 'antd'
+import { UploadOutlined } from '@ant-design/icons';
 
 export const AboutPage = ({
   ...props
 }) => {
 
-  useEffect(() => {
-    console.log("TEST About")
-  },[])
+  const [ fileData, setFileData ] = useState('')
 
+  async function storeBase64(file){
+    var data = new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+    setFileData(await data)
+  }
     
   return (
     <div className="About">
-      <p>About</p>
+      <>
+        <Upload
+          listType="picture"
+          accept=".jpg, .png"
+          beforeUpload={async (file) => {
+              storeBase64(file)
+              return false;
+          }}
+        >
+          <Button icon={<UploadOutlined />}>Upload</Button>
+        </Upload>
+      </>
+      <img style={{maxWidth:'30vw'}} src={fileData}/>
     </div>
   )
 }
