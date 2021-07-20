05140d0d0dc6193ddc1a0ef8466fd1c8d9509726
justinhoejj <hoejj05@gmail.com>
2021-07-20 10:48:50 +0800

    upload button

--- src/pages/chatpage/ChatPage.jsx
+++ src/pages/chatpage/ChatPage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, useContext } from 'react'
-import { Layout, Input, Menu, Button, Form, Row, Col } from 'antd'
+import { Layout, Input, Menu, Button, Form, Row, Col, Upload } from 'antd'
 import { convertToFormData } from '../../constants/URL'
 import axios from 'axios'
 import { errorModal } from '../../components/UI/submissionModal';
+import { UploadOutlined } from '@ant-design/icons';
 
 export const ChatPage = ({
   authContext,
@@ -26,32 +27,32 @@ export const ChatPage = ({
       'Authorization': 'jwt '.concat(token)
     }
     axios.get('api/user/', {
-        headers: headers
-      }).then((res) => {
-        setlistOfRecipients(res.data)
-      })
+      headers: headers
+    }).then((res) => {
+      setlistOfRecipients(res.data)
+    })
       .catch((err) => {
         console.log("ERROR", err)
       });
-  },[])
+  }, [])
 
   const sendMessage = (e) => {
     const data = {
-        fromAddress: user,
-        toAddress: selectedUserId,
-        text:e.message,
-        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJ1c2VybmFtZSI6ImhlbGxvIiwiZXhwIjoxNjI1NzI0NDUxLCJlbWFpbCI6IiJ9.nDNBHhxzT_97R1p8PoEwxYl5PYDnqOurNEmyphz2DzY'
-      }
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'jwt '.concat(token)
-      }
-      axios.post('api/chat/', convertToFormData(data), {
-        headers: headers
-      }).then(()=>{
-        // successModal("Successfully submitted")
-        form.resetFields()
-      })
+      fromAddress: user,
+      toAddress: selectedUserId,
+      text: e.message,
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJ1c2VybmFtZSI6ImhlbGxvIiwiZXhwIjoxNjI1NzI0NDUxLCJlbWFpbCI6IiJ9.nDNBHhxzT_97R1p8PoEwxYl5PYDnqOurNEmyphz2DzY'
+    }
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'jwt '.concat(token)
+    }
+    axios.post('api/chat/', convertToFormData(data), {
+      headers: headers
+    }).then(() => {
+      // successModal("Successfully submitted")
+      form.resetFields()
+    })
       .catch((err) => {
         errorModal(err)
         console.log("ERROR", err)
@@ -63,51 +64,66 @@ export const ChatPage = ({
       'Authorization': 'jwt '.concat(token)
     }
     axios.get('api/chat/', {
-        headers: headers
-      }).then((res) => {
-        setChatHistory(res.data)
-      })
+      headers: headers
+    }).then((res) => {
+      setChatHistory(res.data)
+    })
       .catch((err) => {
         console.log("ERROR", err)
       });
   }, [selectedUserId])
 
+  // File upload
+  const customUpload = options => {
+    const { onSuccess, onError, file, onProgress } = options
+
+    const fmData = new FormData();
+    // const config = {}
+    fmData.append("image", file);
+    axios.post("endpoint", fmData, 
+    // config
+    )
+    .then()
+    .catch()
+  }
 
   return (
     <div className="chat">
       <Layout>
         <Menu>
-        {listOfRecipients.map(item => <Menu.Item key={item.id} onClick={() => setSelectedUserId(item.username)}>{item.username}</Menu.Item>)}
+          {listOfRecipients.map(item => <Menu.Item key={item.id} onClick={() => setSelectedUserId(item.username)}>{item.username}</Menu.Item>)}
         </Menu>
-        </Layout>
-        {chatHistory
-        ?.filter(item => (item.toAddress === user || item.toAddress === selectedUserId) 
+      </Layout>
+      {chatHistory
+        ?.filter(item => (item.toAddress === user || item.toAddress === selectedUserId)
           && (item.fromAddress === user || item.fromAddress === selectedUserId))
         ?.map(item => <li>{item.text}</li>)}
-            <Form
-              form={form}
-              name="chat"
-              onFinish={e => sendMessage(e)}
-            >
-              <Row>
-                <Col span={20}>
-                  <Form.Item
-                    name="message">
-                    <Input
-                      placeholder="Input message"
-                    />
-                  </Form.Item>
-                </Col>
-                <Col span={4}>
-                  <Form.Item>
-                    <Button htmlType='submit'>Send</Button>
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Form>
-          {/* </Footer> */}
-        {/* // </Layout> */}
-      {/* // </Layout> */}
+      <Form
+        form={form}
+        name="chat"
+        onFinish={e => sendMessage(e)}
+      >
+        <Row>
+          <Col span={20}>
+            <Form.Item
+              name="message">
+              <Input
+                placeholder="Input message"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item>
+              <Button htmlType='submit'>Send</Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      <Upload
+      action={customUpload}
+      >
+        <Button icon={<UploadOutlined />}>Upload</Button>
+      </Upload>
     </div>
   )
 }
