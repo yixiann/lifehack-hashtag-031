feeaeb4d17f0b2ca8537efc4040e70e83019ed8a
Donovan <don9617@hotmail.com>
2021-07-21 16:09:08 +0800

    Images

--- backend/lifehack/views.py
+++ backend/lifehack/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions, status
+from django.shortcuts import render
+
+from PIL import Image
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
